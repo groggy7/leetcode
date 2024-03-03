c9199d501d4ddd0eb6c8f556a28bb12905e107b3
Serhat Kılbaş <serhat.kilbas@priviasecurity.com>
2024-03-04 00:37:57 +0300

    Add problem description for valid anagram

--- valid anagram/validAnagram.go
+++ valid anagram/validAnagram.go
@@ -5,6 +5,22 @@ import (
 	"sort"
 )
 
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different
+word or phrase, typically using all the original letters exactly once.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+*/
+
 // Solution 1
 func isAnagram(s string, t string) bool {
 	if len(s) != len(t) {
